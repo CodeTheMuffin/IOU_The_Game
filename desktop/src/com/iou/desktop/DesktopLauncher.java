428be364a839851ad6039b88fd15876f0da01a26
CodeTheMuffin <rt062295@gmail.com>
2018-12-12 11:52:31 -0500

    Setting Up The Game
    Setting up the game

---
+++ desktop/src/com/iou/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.iou.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.iou.The_Main_Game_Class;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new The_Main_Game_Class(), config);
+	}
+}
